98b0f3fe46aacab51c707d2eb99dc1cacba4ff51
Alex <AlexeyVolkov@users.noreply.github.com>
2021-07-14 18:05:07 +0300

    add components boilerplates

---
+++ src/components/Login/Login.js
@@ -0,0 +1,11 @@
+const Login = () => {
+  return (
+    <form>
+      <fieldset>
+        <input type='email' />
+        <input type='password' />
+      </fieldset>
+    </form>
+  )
+}
+export default Login
